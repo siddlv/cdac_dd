efab0a571b580d7f5ca18de81ea2ccf9fb9893c4
Siddhesh Dalvi <dalvisiddhesh09@gmail.com>
2019-12-14 19:14:11 +0530

    Simple character device driver

---
+++ chardev/chardev.c
@@ -0,0 +1,45 @@
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/kernel.h>
+#include<linux/cdev.h>
+#include<linux/fs.h>
+
+dev_t dev_no;
+struct cdev c_dev;
+struct file_operations fops = {};
+int ret;
+
+static int __init chardev_init(void){
+
+	printk(KERN_ALERT "\nIn chardev init function\n");
+	
+	dev_no = MKDEV(42, 0);
+	
+	ret = register_chrdev_region(dev_no, 1, "chardev");
+	if(ret){
+		printk("\nError registering driver\n");
+	}
+	
+	cdev_init(&c_dev, &fops);
+	
+	ret = cdev_add(&c_dev, dev_no, 1);
+	if(ret){
+		printk("\nError adding cdev\n");
+	}
+        
+	return 0;
+}
+
+static void __exit chardev_exit(void){
+        
+	printk(KERN_ALERT "\nIn chardev exit function\n");
+	
+	cdev_del(&c_dev);
+	
+	unregister_chrdev_region(dev_no, 1);
+}
+
+module_init(chardev_init);
+module_exit(chardev_exit);
+
+
