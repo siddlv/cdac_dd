04fe8bbf8de7da9af4e06fc7aa4e8e34bad5e14c
Siddhesh Dalvi <dalvisiddhesh09@gmail.com>
2019-12-18 07:47:39 +0530

    Added author and license to the driver code and better cleanup code on driver exit

--- char_rw_dy/Application.c
+++ char_rw_dy/Application.c
@@ -4,19 +4,20 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
 int main()
 {
 	int FileDesc;
-	char Ubuff[]="User Buffer: Data from User Space";
-	char Kbuff[100];	
-	FileDesc=open("/dev/MyCharDevice",O_RDWR,0777);
-	if(FileDesc <0)
+	char Ubuff[] = "User Buffer: Data from User Space";
+	char Kbuff[100];
+	FileDesc = open("/dev/MyCharDevice", O_RDWR, 0777);
+	if (FileDesc < 0)
 	{
-		printf("\nError Opening Device\n");	
+		printf("\nError Opening Device\n");
 		exit(1);
 	}
-	write(FileDesc,Ubuff,sizeof(Ubuff));
-	read(FileDesc,Kbuff,sizeof(Kbuff));
-	printf("\nData From Kernel\n%s\n",Kbuff);
+	write(FileDesc, Ubuff, sizeof(Ubuff));
+	read(FileDesc, Kbuff, sizeof(Kbuff));
+	printf("\nData From Kernel\n%s\n", Kbuff);
 	close(FileDesc);
 }
--- char_rw_dy/CharDriver.c
+++ char_rw_dy/CharDriver.c
@@ -5,6 +5,10 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
+
+MODULE_LICENSE("GPL v3.0");
+MODULE_AUTHOR("Siddhesh Dalvi");
+
 #define NAME MyCharDevice
 
 //Function Prototypes
@@ -14,10 +18,10 @@ ssize_t NAME_write(struct file *filp, const char __user *Ubuff, size_t count, lo
 ssize_t NAME_read(struct file *filp, char __user *Ubuff, size_t count, loff_t  *offp);
 
 //Structure that defines the operations that the driver provides
-struct file_operations fops = 
+struct file_operations fops =
 {
 	.owner   = THIS_MODULE,
-	.open    = NAME_open, 
+	.open    = NAME_open,
 	.read    = NAME_read,
 	.write   = NAME_write,
 	.release = NAME_release
@@ -25,36 +29,38 @@ struct file_operations fops =
 
 //Structure for a character driver
 struct cdev *my_cdev;
+dev_t Mydev;
+int MAJOR, MINOR;
 
 //Init Module
 static int __init CharDevice_init(void)
 {
 	int result;
-	dev_t Mydev;
-	 
-	result = alloc_chrdev_region(&Mydev, 0, 1,"MyCharDevice");
+
+	result = alloc_chrdev_region(&Mydev, 0, 1, "MyCharDevice");
 	if (result < 0) {
 		printk(KERN_WARNING "\nMyCharDevice: Can't get major device\n");
 		return result;
 	}
-		
-	result = register_chrdev_region(0,1,"MyCharDevice");//register device region.....
-	
-	if(result<0)
+
+	result = register_chrdev_region(0, 1, "MyCharDevice"); //register device region.....
+
+	if (result < 0)
 	{
 		printk(KERN_ALERT "\nThe region requested is not obtainable\n");
-		return(-1);
+		return (-1);
 	}
-	printk("\nMajor Number: %d Minor Number: %d\n",MAJOR(Mydev),MINOR(Mydev));
+	printk("\nMajor Number: %d Minor Number: %d\n", MAJOR(Mydev), MINOR(Mydev));
 	printk("\nDevice registered\n");
 
-	my_cdev = cdev_alloc();//allocate memory to Char Device structure 
+	my_cdev = cdev_alloc();//allocate memory to Char Device structure
 	my_cdev->ops = &fops;//link our file operations to the char device
 
-	result=cdev_add(my_cdev,Mydev,1);//Notify the kernel abt the new device
-	if(result<0)
+	result = cdev_add(my_cdev, Mydev, 1); //Notify the kernel abt the new device
+	if (result < 0)
 	{
 		printk(KERN_ALERT "\nChar Device is not created\n");
+		unregister_chrdev_region(Mydev, 1);
 		return (-1);
 	}
 	return 0;
@@ -63,13 +69,8 @@ static int __init CharDevice_init(void)
 //Cleanup Module
 void __exit CharDevice_exit(void)
 {
-	dev_t Mydev;
-	int MAJOR,MINOR;
-	Mydev=MKDEV(255,0);
-	MAJOR=MAJOR(Mydev);
-	MINOR=MINOR(Mydev);
-	printk("\nMajor Number: %d Minor Number: %d\n",MAJOR,MINOR);
-	unregister_chrdev_region(Mydev,1);//unregister the device numbers and the device created
+	printk("\nMajor Number: %d Minor Number: %d\n", MAJOR, MINOR);
+	unregister_chrdev_region(Mydev, 1); //unregister the device numbers and the device created
 	cdev_del(my_cdev);
 	printk(KERN_ALERT "\nDevice unregisterd\n");
 	return;
@@ -87,7 +88,7 @@ int NAME_release(struct inode *indoe, struct file *filp)
 {
 	printk(KERN_ALERT "\nKernel Release Call\n");
 	return 0;
-} 
+}
 
 //Write Functionality
 ssize_t NAME_write(struct file *filp, const char __user *Ubuff, size_t count, loff_t  *offp)
@@ -96,42 +97,42 @@ ssize_t NAME_write(struct file *filp, const char __user *Ubuff, size_t count, lo
 	unsigned long result;
 	ssize_t retval;
 	//strcpy(Kbuff,Ubuff);
-	result=copy_from_user((char *)Kbuff,(char *)Ubuff,count); //get user data
-	if(result==0)
+	result = copy_from_user((char *)Kbuff, (char *)Ubuff, count); //get user data
+	if (result == 0)
 	{
-		printk(KERN_ALERT "\nUser Message: \n%s\n",Kbuff);
-		printk(KERN_ALERT "\nKernel Read Successful\n");	
-		retval=count;
+		printk(KERN_ALERT "\nUser Message: \n%s\n", Kbuff);
+		printk(KERN_ALERT "\nKernel Read Successful\n");
+		retval = count;
 		return retval;
 	}
 	else
 	{
 		printk(KERN_ALERT "\nError Writing Data\n");
-		retval=-EFAULT;
+		retval = -EFAULT;
 		return retval;
 	}
 }
 
-//Read Functionality	
+//Read Functionality
 ssize_t NAME_read(struct file *filp, char __user *Ubuff, size_t count, loff_t  *offp)
 {
-	char Kbuff[]="Kernel Buffer: Data from Kernel";
+	char Kbuff[] = "Kernel Buffer: Data from Kernel";
 	unsigned long result;
 	ssize_t retval;
-	result=copy_to_user((char *)Ubuff,(char *)Kbuff,sizeof(Kbuff)); //copy to user
-	if(result==0)
+	result = copy_to_user((char *)Ubuff, (char *)Kbuff, sizeof(Kbuff)); //copy to user
+	if (result == 0)
 	{
-		printk(KERN_ALERT "\nKernel Write Succesful\n");	
-		retval=count;
+		printk(KERN_ALERT "\nKernel Write Succesful\n");
+		retval = count;
 		return retval;
 	}
 	else
 	{
 		printk(KERN_ALERT"\nError Reading Data\n");
-		retval=-EFAULT;
+		retval = -EFAULT;
 		return retval;
 	}
-}	
+}
 
 //Module over ride functions
 module_init(CharDevice_init);
